72edf80f680666a9c62a152544bbfd2f2f4bb202
firoz1860@gmail.com <99130904+FIR2@users.noreply.github.com>
2024-04-04 07:49:39 +0530

    new json data added

--- index.js
+++ index.js
@@ -9,12 +9,21 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+data = {
+    1:"firox",
+    2:"ansari"
+}
+
 app.get('/twitter', (req, res) =>{
     res.send("Hello Twitter")
 })
 
 app.get('/login',(req,res) => {
     res.send("<h1>this is login page.</h1>")
+})
+
+app.get('/jsondata', (req,res) => {
+    res.json(data)
 })
 
 app.listen(process.env.port, () => {
